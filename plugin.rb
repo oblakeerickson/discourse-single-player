1912463f2c4b7a5c94e05af31dd0b51c177a9c17
Blake Erickson <o.blakeerickson@gmail.com>
2023-09-02 07:41:20 -0600

    Create user group on user create

--- plugin.rb
+++ plugin.rb
@@ -19,6 +19,22 @@ after_initialize do
   # Code which should run after Rails has finished booting
   DiscourseEvent.on(:user_created) do |user|
     puts "USER CREATED !!!!!!!!!!!!!"
+    puts "USERNAME: #{user.username}"
+    puts "INSPECT: "
     puts user.inspect
+
+    username = user.username
+    group_name = "g-#{username}"[0..19]
+    g = Group.create!({
+      name: group_name,
+      visibility_level: 4,
+      members_visibility_level: 4,
+    })
+
+    puts "GROUP: #{g.name}"
+    puts g.inspect
+
+    # can see - Group owners
+    # can see members - Group owners
   end
 end
